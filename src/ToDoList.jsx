88c3287dc5488a293f38c7077949a28a3765eb5c
technomancer <bozios11@gmail.com>
2024-12-11 15:06:22 +0100

    second commit added buttons and css

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useState }  from 'react'
 
 function ToDoList(){
-    const[tasks,setTasks] = useState([]);
+    const[tasks,setTasks] = useState(["walk the dog","make a fight","over throw goverment"]);
     const[newTask,setNewTask] = useState("");
 
     function handleInputChange(event){
@@ -25,13 +25,28 @@ function ToDoList(){
     }
   return(
     <>
-        <div>
+        <div className="to-do-list">
             <h1>To-Do-List</h1>
             <div>
                 <input type="text"
                        placeholder="Enter task..."
                        value={newTask}
                        onChange={handleInputChange} />
+                 <button 
+                     className="add-button"
+                     onClick={addTask}>
+                      Add 
+                     </button>
+                  <ol>
+                      {tasks.map((element,index) => 
+                       <li key={index}>
+                           <span className="text">{element}</span>
+                           <button className='delete-button' onClick={() => deleteTask(index)}>delete</button>
+                           <button className="up-button" onClick={() => moveTaskUp(index)}>UP</button>
+                           <button className="down-button" onClick={() => moveTaskDown(index)}>DOWN</button>
+                       </li>
+                      )}
+                  </ol>
             </div>
         </div>
     </>
